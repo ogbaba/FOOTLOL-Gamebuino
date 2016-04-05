a14297e7cd1fdbf1cad8d85dd1641e3e036ae11a
baptistepouget <baptistepouget@tuta.io>
2016-04-05 16:42:31 +0200

    better

--- Footlol/Footlol.ino
+++ Footlol/Footlol.ino
@@ -1,24 +1,32 @@
 #include <Gamebuino.h>
 #include <SPI.h>
+#include "Pos.c"
 
-#define TERRAING 12
-#define CAGESH 14
-#define RBALLE 2
+Gamebuino gb;
+
+typedef struct Joueur {
+  int x;
+  int y;
+  char equipe;
+} Joueur;
+
+Joueur Joueurs[NJOUEURS];
+
 int ballePosX = LCDWIDTH/2;
 int ballePosY = LCDHEIGHT/2;
 
 
-void dDecor();
 void balle();
 void dJoueurs();
-void initialiser();
 void collisions();
+void initialiser();
+void dDecor();
 
 
-Gamebuino gb;
 void setup() {
   // put your setup code here, to run once:
   gb.begin();
+  initialiser();
   gb.titleScreen(F("Footlol"));
 
 }
@@ -29,7 +37,10 @@ void loop() {
   dDecor();
   balle();
   dJoueurs();
-  if (gb.buttons.pressed(BTN_C)) {gb.titleScreen(F("Footlol"));}
+  if (gb.buttons.pressed(BTN_C)) {
+    gb.titleScreen(F("Footlol"));
+    initialiser();
+    }
   }
 }
 
@@ -46,6 +57,9 @@ gb.display.drawLine(LCDWIDTH - TERRAING,LCDHEIGHT-CAGESH,LCDWIDTH - TERRAING,LCD
 gb.display.drawLine(LCDWIDTH,LCDHEIGHT - CAGESH,LCDWIDTH - TERRAING,LCDHEIGHT - CAGESH);
 //Le centre du terrain
 gb.display.drawLine(LCDWIDTH/2,0,LCDWIDTH/2,LCDHEIGHT);
+// Les scores toussa
+gb.display.drawChar(0,LCDHEIGHT-gb.display.10,'B',10);
+gb.display.drawChar(0,LCDHEIGHT-gb.display.10,'B',10);
 }
 
 void balle(){
@@ -53,12 +67,41 @@ void balle(){
   
 }
 void dJoueurs(){
-  
+  for(int i = 0; i < NJOUEURS; i++)
+  {
+    switch (Joueurs[i].equipe){
+      case 'w':
+      gb.display.fillCircle(Joueurs[i].x, Joueurs[i].y, RJOUEUR);
+      break;
+      case 'b':
+      gb.display.drawCircle(Joueurs[i].x, Joueurs[i].y, RJOUEUR);
+      break;
+    }
+    
+  }
 }
 
 void initialiser(){
   ballePosX = LCDWIDTH/2;
   ballePosY = LCDHEIGHT/2;
+  Joueurs[0].x = J1X;
+  Joueurs[0].y = J1Y;
+  Joueurs[0].equipe = 'w';
+  Joueurs[1].x = J2X;
+  Joueurs[1].y = J2Y;
+  Joueurs[1].equipe = 'w';
+  Joueurs[2].x = J3X;
+  Joueurs[2].y = J3Y;
+  Joueurs[2].equipe = 'w';
+  Joueurs[3].x = J4X;
+  Joueurs[3].y = J4Y;
+  Joueurs[3].equipe = 'b';
+  Joueurs[4].x = J5X;
+  Joueurs[4].y = J5Y;
+  Joueurs[4].equipe = 'b';
+  Joueurs[5].x = J6X;
+  Joueurs[5].y = J6Y;
+  Joueurs[5].equipe = 'b';
 }
 
 void collisions(){
---
+++ Footlol/Pos.c
@@ -0,0 +1,18 @@
+#define TERRAING 12
+#define CAGESH 14
+#define RBALLE 2
+
+#define NJOUEURS 6
+#define RJOUEUR 3
+#define	J1X 84/2-10
+#define J1Y 48/2-8
+#define	J2X 84/2-10
+#define J2Y 48/2+8
+#define J3X 84/2-20
+#define J3Y 48/2
+#define J4X 84/2+10
+#define J4Y 48/2-8
+#define J5X 84/2+10
+#define J5Y 48/2+8
+#define J6X 84/2+20
+#define	J6Y 48/2
